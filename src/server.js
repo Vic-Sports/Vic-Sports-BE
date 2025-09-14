c7174efbb38cf1b4f34850df367322df4a668c0d
An Nguyen <anndb1903@gmail.com>
2025-09-14 19:18:34 +0700

    route

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import connectDB from './config/database.js';
 import logger from './utils/logger.js';
 import { corsMiddleware } from './config/cors.config.js';
 import { errorHandler } from './middlewares/error.middleware.js';
-import apiRoutes from './routes/api.js';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import compression from 'compression';
@@ -31,7 +30,28 @@ app.get('/', (req, res) => {
 });
 
 // Routes
-app.use('/api/v1', apiRoutes);
+import authRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import venueRoutes from "./routes/venue.route.js";
+import courtRoutes from "./routes/court.route.js";
+import bookingRoutes from "./routes/booking.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import coachRoutes from "./routes/coach.route.js";
+import loyaltyRoutes from "./routes/loyalty.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+router.use("/api/v1/auth", authRoutes);
+router.use("/api/v1/users", userRoutes);
+router.use("/api/v1/admin", adminRoutes);
+router.use("/api/v1/venues", venueRoutes);
+router.use("/api/v1/courts", courtRoutes);
+router.use("/api/v1/bookings", bookingRoutes);
+router.use("/api/v1/reviews", reviewRoutes);
+router.use("/api/v1/chats", chatRoutes);
+router.use("/api/v1/coaches", coachRoutes);
+router.use("/api/v1/loyalty", loyaltyRoutes);
+router.use("/api/v1/analytics", analyticsRoutes);
 
 // Error handler
 app.use(errorHandler);
